6a516daa6610b293303bf4c4c006566a4c96eee6
anjalitestgit <41549368+anjalitestgit@users.noreply.github.com>
2018-07-29 13:50:26 +0530

    Initial commit

---
+++ A.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+class A
+{
+	public static void main(String ar[])
+	{
+		
+		String s[]=ar[0].split("\\\\");
+		String s1[]=ar[1].split("\\\\");
+		//for(String s1:s)
+		for(int i=0;i<s.length;i++)
+		{
+
+			//System.out.println(s1);
+		
+			File dr=new File("c:\\javadir");
+			System.out.println(dr.isDirectory());
+			i++;
+			System.out.println(s[i]);
+			File f=new File(dr,s[i]);
+			boolean b=f.isFile();
+			System.out.println(b);
+			if(b)
+			{
+			  String ex[]=s[i].split("\\.");						  System.out.println("ex="+ex[1]);
+			  System.out.println(s[i]);
+			  if(ex[1].equals("txt"))
+			 {
+				f.delete();	
+				System.out.println("deleted");
+			 }	
+			}
+		}
+		for(int i=0;i<s1.length;i++)
+		{
+
+			//System.out.println(s1);
+		
+			File dr=new File("javadir");
+			System.out.println(dr.isDirectory());
+			i++;
+			System.out.println(s1[i]);
+			File f=new File(dr,s1[i]);
+			System.out.println(f.isFile());
+		}
+
+	}
+
+
+}
---
+++ A1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+class A1
+{
+	public static void main(String ar[])
+	{
+		File f=new File("c:\\javadir");
+		String s[]=f.list();
+		for(int i=0;i<s.length;i++)
+		{	
+			String s1=s[i];
+			System.out.println(s1);
+			File old=new File(f,s1);
+			String s2=s1+".html";
+			File new1=new File(f,s2);
+			old.renameTo(new1);
+		}
+	}
+
+
+}		
